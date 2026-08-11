ac4f8c8ee4cf47f2d33f52279a18e2fb5524d4c9
Jun-R <taejun3996@gmail.com>
2026-08-11 00:51:05 -0400

    removed redundant button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,16 +14,6 @@ export default function About() {
               <p className="text-sm/6 opacity-85">
                 1인 원장이 진료하는 미톡스 클리닉은 고객의 청춘과 건강을 위한 체계적이고 합리적인 접근으로 가시적인 결과를 가져오는 환상적인 경험을 선사합니다!
               </p>
-              <div className="flex gap-3 pt-2">
-                <a
-                  href="https://map.naver.com/p/entry/place/1225820706?lng=127.1145353&lat=37.5051720&placePath=%2Fbooking%3FbookingRedirectUrl%3Dhttps%3A%2F%2Fm.booking.naver.com%2Fbooking%2F13%2Fbizes%2F1053453%3Ftheme%3Dplace%26entry%3Dpll%26lang%3Dko%26entry%3Dpll&area=pll&c=15.00,0,0,0,dh"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="diag-hover inline-flex items-center justify-center rounded-none px-5 py-2.5 text-sm font-semibold text-white bg-[color:var(--pink-600)] hover:brightness-110"
-                >
-                  예약하기
-                </a>
-              </div>
             </div>
 
             <div className="relative min-w-0">
